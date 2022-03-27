a02279ad5b52090ebf176d9a5b32abecd40369c6
Manas Nikte <manas.nikte@gmail.com>
2022-03-27 18:59:41 +0530

    adding port

---
+++ controllers/image.js
@@ -0,0 +1,36 @@
+const Clarifai = require('clarifai');
+const { json } = require('express/lib/response');
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+  apiKey: '33b5f868c7254d65ae1eea1193b99780'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data =>{
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+      // entries[0] --> this used to return the entries
+      // TO
+      // entries[0].entries --> this now returns the entries
+      res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+  }
+
+  module.exports = {
+    handleImage,
+    handleApiCall
+  }
