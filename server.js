6f275dbe39df9e86ebd8d09db81be823a694f37e
Lylia <71930916+dishapatel010@users.noreply.github.com>
2022-04-09 18:34:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,13 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.put('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin','*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
+  next();
+});
 
 app.listen(process.env.PORT || 3000, ()=> {
   console.log(`app is running on port ${process.env.PORT}`);
